a03b4fbf42b8c7491f42521b963a747e253fbec8
demartm <artemis20242511@gmail.com>
2026-05-19 00:09:58 -0400

    offsets fixed and bool->void

--- main.c
+++ main.c
@@ -4,154 +4,167 @@
 
 struct point {
 
-        int x;
-        int y;
-        struct point* prev;
-        struct point* next;
+      int x;
+      int y;
+      struct point* prev;
+      struct point* next;
 };
 
 struct point* addBefore(struct point **list,int x,int y,int x_add,int y_add) {
-        if (list) {
-                struct point* el = (struct point*)calloc(sizeof(struct point), 1);
-                if (el) {
-                        struct point* cur = NULL;// (*list);
-                        for (cur = (*list); cur && !(cur->x == x && cur->y == y); cur = cur->next);
+if (list) {
 
-                        if (cur) {
-                                el->x = x_add;
-                                el->y = y_add;
+  struct point* el = (struct point*)calloc(sizeof(struct point), 1);
+  if (el) {
 
-                                        el->prev = cur->prev;
-                                        el->next = cur;
+    struct point* cur = NULL;// (*list);
+    for (cur = (*list); cur && !(cur->x == x && cur->y == y); cur = cur->next);
 
-                                //}
-                                        if (cur->prev == NULL) {
-                                                el->prev = NULL;////
-                                                cur->prev = el;
-                                                (*list) = el;
-                                        }
-                                        else {
-                                                el->prev = cur->prev;
-                                                cur->prev->next = el;
-                                                cur->prev = el;
-                                        }
+    if (cur) {
+      el->x = x_add;
+      el->y = y_add;
 
+      el->prev = cur->prev;
+      el->next = cur;
 
+      if (cur->prev == NULL) {
 
-                        }
-                        else {
-                                free(el);
-                        }
+        el->prev = NULL;
+        cur->prev = el;
+        (*list) = el;
+      }
+      else {
 
-
-                }
+        el->prev = cur->prev;
+        cur->prev->next = el;
+        cur->prev = el;
+      }
 
 
 
-        }
+    }
+    else {
+    free(el);
+    }
 
-        return NULL;
+  }
 }
+
+return NULL;
+}
+
 struct point *addAfter(struct point *list,int x,int y,int x_add,int y_add) {
-        if (list) {
-                struct point *el = (struct point*)calloc(sizeof(struct point), 1);
+if (list) {
+struct point *el = (struct point*)calloc(sizeof(struct point), 1);
 
-                if (el) {
+if (el) {
 
-                        while (list && !(list->x == x && list->y == y)) {
-                                list = list->next;
-                        }
-                        if (list) {
-                                el->x = x_add;
-                                el->y = y_add;
+  while (list && !(list->x == x && list->y == y)) {
+    list = list->next;
+  }
 
-                                        el->prev = list;
-                                        el->next = list->next;
-                                        list->next = el;
-                                        if (el->next) {
-                                                el->next->prev = el;
-                                        }
+  if (list) {
+    el->x = x_add;
+    el->y = y_add;
 
-                        }
-                        else {
-                                free(el);
-                        }
+    el->prev = list;
+    el->next = list->next;
+    list->next = el;
 
-                }
+    if (el->next) {
+
+      el->next->prev = el;
+    }
+
+  }
+  else {
+    free(el);
+  }
+
+  }
 
 
-        }
+}
 
 
-        return NULL;
+return NULL;
 }
 struct point* find(struct point *head,int x,int y) {
-        if (head) {
-                while (head && !(head->x == x && head->y == y)) {
-                        head = head->next;
-                }
-                if (head) {
-                        return head;
-                }
+if (head) {
+  while (head && !(head->x == x && head->y == y)) {
 
+    head = head->next;
+  }
+  if (head) {
 
-        }
-
-
-        return NULL;
+    return head;
+  }
 }
-bool delHead(struct point** head) {
-        if (head && *head) {
-                struct point* ptr = (*head)->next;
-                if (ptr) {
-                        (*head)->next->prev = NULL;
-                }
-                free(*head);
-                (*head) = ptr;
+return NULL;
+}
 
-                return true;
-        }
+void delHead(struct point** head) {
+if (head && *head) {
 
-        return false;
+  struct point* ptr = (*head)->next;
+
+  if (ptr) {
+
+    (*head)->next->prev = NULL;
+  }
+  free(*head);
+  (*head) = ptr;
+
+}
+
+return;
 }
 void delAtPos(struct point **list,size_t pos) {
-        if (list && *list) {
-                struct point* cur = NULL;// *list;
-                size_t i = 0;
-                for (cur = *list; i < pos && cur; cur = cur->next, i++);
+if (list && *list) {
 
-                if (cur && i == pos) {
+struct point* cur = NULL;// *list;
 
-                        if (cur->prev) {
-                                if (cur->next) {
-                                        cur->prev->next = cur->next;
-                                        cur->next->prev = cur->prev;
-                                        free(cur);
-                                }
-                                else {
-                                        cur->prev->next = NULL;
-                                        free(cur);
-                                }
-                        }
-                        else {
-                                if (cur->next) {
-                                        cur->next->prev = NULL;
-                                        (*list) = cur->next;
-                                        free(cur);
-                                }
-                                else {
-                                        free(cur);
-                                        (*list) = NULL;
-                                }
-                        }
+size_t i = 0;
 
-                }
+for (cur = *list; i < pos && cur; cur = cur->next, i++);
+
+if (cur && i == pos) {
+
+  if (cur->prev) {
+
+    if (cur->next) {
+
+      cur->prev->next = cur->next;
+      cur->next->prev = cur->prev;
+      free(cur);
+    }
+    else {
+
+      cur->prev->next = NULL;
+      free(cur);
+    }
+  }
+  else {
+
+    if (cur->next) {
+
+      cur->next->prev = NULL;
+      (*list) = cur->next;
+      free(cur);
+    }
+    else {
+
+      free(cur);
+      (*list) = NULL;
+    }
+  }
+
+}
 
 
-        }
+}
 
 
-        return;
+return;
 }
 
 int main(){
