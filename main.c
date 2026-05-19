cf180a37fd045f5bb1d29bb6c6ee62488bdc4286
demartm <artemis20242511@gmail.com>
2026-05-18 23:59:47 -0400

    function structure changed

--- main.c
+++ main.c
@@ -12,67 +12,68 @@ struct point {
 
 struct point* addAtPos(struct point** list, int x, int y, size_t pos) {
 
-        if (list) {
+if (list) {
 
-                struct point* el = (struct point*)calloc(sizeof(struct point), 1);
+  struct point* el = (struct point*)calloc(sizeof(struct point), 1);
 
-                if (el) {
-                        el->x = x;
-                        el->y = y;
+  if (el) {
+    el->x = x;
+    el->y = y;
+    if (!(*list) && pos == 0) {
+      (*list) = el;
+      return el;
+    }
 
-                        if (!(*list) && pos == 0) {
-                                (*list) = el;
-                                return el;
-                        }
+    size_t i = 0;
 
-                        size_t i = 0;
-                        struct point *cur = NULL;
-                        struct point* previous = NULL;
-                        for (cur = *list; cur && i < pos; cur = cur->next, i++) {
-                                if (cur->prev) {
-                                        previous = cur;// ->prev;
-                                }
-                        }
+    struct point *cur = NULL;
+    struct point* previous = NULL;
 
-                        if (i == pos) {
+    for (cur = *list; cur && i < pos; cur = cur->next, i++) {
 
-                                if (cur) {
-                                        el->next = cur;
+      if (cur->prev) {
 
-                                        if (cur->prev) {
-                                                cur->prev->next = el;
-                                        }
-                                        else {
-                                                (*list) = el;
-                                        }
+        previous = cur;// ->prev;
+      }
+    }
+    if (i == pos) {
 
+    if (cur) {
+      el->next = cur;
 
-                                }
-                                else {
-                                        if (previous) {
-                                                previous->next = el;
-                                                el->prev = previous;
-                                                el->next = NULL;
-                                        }
+      if (cur->prev) {
 
-                                }
+        cur->prev->next = el;
+      }
+      else {
 
-                                return el;
-                        }
-                        else {
-                                free(el);
-                        }
+        (*list) = el;
+      }
 
 
+    }
+    else {
 
-                }
+      if (previous) {
 
+        previous->next = el;
+        el->prev = previous;
+        el->next = NULL;
+      }
 
+    }
 
+    return el;
+    }
+    else {
+      free(el);
+    }
 
-        }
+  }
 
-        return NULL;
+}
+
+return NULL;
 }
 
 int main(){
