3de52a38649344c14c4d086f663d3dc7dc211421
demartm <artemis20242511@gmail.com>
2026-05-18 23:34:59 -0400

    functions created

--- main.c
+++ main.c
@@ -1,7 +1,94 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-int main()
-{
-    printf("Hello World!\n");
-    return 0;
+struct point {
+
+        int x;
+        int y;
+        struct point* prev;
+        struct point* next;
+};
+
+
+bool addToHead(struct point** head,int x, int y) {
+        if (head/* && *head*/) {
+                struct point* new_head = (struct point*)calloc(1, sizeof(struct point));
+                if (new_head) {
+                        new_head->x = x;
+                        new_head->y = y;
+                        new_head->next = (*head);
+                        new_head->prev = NULL;
+                        if (*head) {
+                                (*head)->prev = new_head;
+                        }
+                        (*head) = new_head;
+                        return true;
+                }
+        }
+
+        return false;
+}
+
+struct point* addToTail(struct point **head,int x,int y) {
+        if (head) {
+                struct point* list = *head;
+                struct point* new_tail = (struct point*)calloc(sizeof(struct point), 1);
+                if (new_tail) {
+                        if (list) {
+                                while (list->next) {
+                                        list = list->next;
+                                }
+
+                                list->next = new_tail;
+                        }
+                        else {
+                                (*head) = new_tail;
+                        }
+                        new_tail->x = x;
+                        new_tail->y = y;
+                        new_tail->prev = list;
+                        new_tail->next = NULL;
+
+
+
+                        return new_tail;
+
+                }
+        }
+
+
+        return NULL;
+}
+
+bool delTail(struct point **list) {
+        if (list && *list) {
+                struct point* cur = *list;
+                while (cur->next) {
+                        //printf("%d %d\n", list->x, list->y);
+                        cur = cur->next;
+                }
+                //list->prev->next = NULL;
+                if (!(cur->prev)) {
+
+                        free(*list);
+                        *list = NULL;
+                }
+                else {
+                        cur->prev->next = NULL;
+                        free(cur);
+
+                }
+
+                return true;
+
+
+
+        }
+
+        return false;
+}
+
+int main() {
+return 0;
 }
